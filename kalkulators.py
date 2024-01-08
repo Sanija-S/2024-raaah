e62754692c8b2bb31a6ee25ff1844014eb18c603
Sanija Sokirka <sanijasokirka@gmail.com>
2024-01-08 09:36:27 +0200

    labots kalkīts - paldes Ance

--- kalkulators.py
+++ kalkulators.py
@@ -1,6 +1,6 @@
 # ka nolasa faila daus csv json utt
 #izveidojam loga izkartojumu
-import PysimpleGUI as sg
+import PySimpleGUI as sg
 #paligmainigie, kuri uzglaba ievades datus
 # iekseja funkc kas veic aprekinus
 def calculate(num1,num2,opperation):
@@ -11,12 +11,15 @@ def calculate(num1,num2,opperation):
          rezultats= num1+num2
       elif opperation =="-":
          rezultats= num1-num2
+      else:
+         rezultats=None
+         return rezultats
    except ValueError: #paredz ka pie kadas darb var rasties kluuda
       return "kĻūda"
 current_input=""
 current_opperation=""
 layout=[
-    [sg.InputText(key="-DISPLAY-", size=(20,1), justificator="right", readonly=True)],
+    [sg.InputText(key="-DISPLAY-", size=(20,1), justification="right", readonly=True)],
     {sg.Button("1"), sg.Button("2"),sg.Button("3"),sg.Button("+")},
     {sg.Button("4"), sg.Button("5"),sg.Button("6"),sg.Button("-")},
     {sg.Button("7"), sg.Button("8"),sg.Button("9"),sg.Button("C")},
@@ -31,7 +34,7 @@ window=sg.Window("Kalkulators", layout, resizable= True)
 # balstoties uz notkumiem veidosim ciklu, like previous lināa nekas nenotiek
 while True:
     # ir notikumi, vertibas
-    event, values=window.read # si metode tikai nolasa logu ar visam vertibam
+    event, values=window.read() # si metode tikai nolasa logu ar visam vertibam
 # LOGS JAIZTAISA CIet
     
     # ja notiek aizversanas notikums vai poga EXIT, tad iziet no cikla(aizver logu)
