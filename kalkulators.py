130d85d47e662f43a1f4de3bb0ebfa03cc57b4c6
Sanija Sokirka <sanijasokirka@gmail.com>
2024-01-08 09:03:36 +0200

    2h darbs

--- kalkulators.py
+++ kalkulators.py
@@ -1,8 +1,17 @@
 # ka nolasa faila daus csv json utt
 #izveidojam loga izkartojumu
 import PysimpleGUI as sg
+#paligmainigie, kuri uzglaba ievades datus
+current_imput=""
+current_opperation=""
 layout=[
-    [sg.InputText(key="-DISPLAY-", size=(20,1), justificator="right", readonly=True)]
+    [sg.InputText(key="-DISPLAY-", size=(20,1), justificator="right", readonly=True)],
+    {sg.Button("1"), sg.Button("2"),sg.Button("3"),sg.Button("+")},
+    {sg.Button("4"), sg.Button("5"),sg.Button("6"),sg.Button("-")},
+    {sg.Button("7"), sg.Button("8"),sg.Button("9"),sg.Button("c")},
+    {sg.Button("0"), sg.Button("="),sg.Button("EXIT")}
+
+
 
 ]
 # izveidosim logu ar ieprieks definetu izkartojumu
@@ -13,4 +22,12 @@ while True:
     # ir notikumi, vertibas
     event, values=window.read # si metode tikai nolasa logu ar visam vertibam
 # LOGS JAIZTAISA CIet
-    window.close
+    
+    # ja notiek aizversanas notikums vai poga EXIT, tad iziet no cikla(aizver logu)
+
+    if event==sg.WIN_CLOSED or event=="EXIT":#pie pogas exit atgriezas sei tun pievieno notikumu
+        break
+    elif
+    # ja ciparpoga ir nos
+    # pista tad to pievieno teksta ievades laukā
+    window.close()
