b6cf2afc8d79077d0c23c55b85143a9bd8d11aac
Sanija Sokirka <sanijasokirka@gmail.com>
2024-01-08 10:22:22 +0200

    atjaunota real versija- tikai nez kas ar ikonnu

--- kalkulators.py
+++ kalkulators.py
@@ -65,10 +65,17 @@ while True:
     
     window.close()
     """
+
+
 # skolotajas piemers/ risinajums
+
+
+"""
 # Iekšējā funkcija, kas veic aprēķinus
 import PySimpleGUI as sg
-sg.theme("Lightgreen2")
+sg.theme("LightGreen2")
+#pip install pyinstaller
+#pyinstaller  --onefile --windowed -- icon= star.ico kalkulators.py
 def calculate(num1, num2, operation):
     try:
         num1 = float(num1)
@@ -97,12 +104,11 @@ layout = [
     [sg.Button('1'), sg.Button('2'), sg.Button('3')],
     [sg.Button('4'), sg.Button('5'), sg.Button('6')],
     [sg.Button('7'), sg.Button('8'), sg.Button('9'), sg.Button('0')],
-     [sg.Button('+'), sg.Button('-'), sg.Button('*')],
-    [sg.Button('=')],
+     [sg.Button('+'), sg.Button('-'), sg.Button('*'),sg.Button('=')],
    
 ]
 
- 
+
 # Izveidojiet logu ar iepriekš definēto izkārtojumu
 window = sg.Window('Kalkulators', layout, resizable=True)
  
@@ -127,6 +133,88 @@ while True:
  
     # Ja "C" poga tiek nospiesta, notīra teksta ievades lauku un darbību
     elif event == 'Delete':
+        current_input = ''
+        current_operation = ''
+        window['-DISPLAY-'].update(current_input)
+ 
+    # Ja "=" poga tiek nospiesta, veic kalkulācijas un atjauno teksta ievades lauku ar rezultātu
+    elif event == '=':
+        result = calculate(current_input, values['-DISPLAY-'], current_operation)
+        window['-DISPLAY-'].update(result if result is not None else '')
+        current_input = str(result) if result is not None else ''
+ 
+# Aizveriet GUI logu
+window.close()
+    """
+
+
+
+#atjaunota real versija- tikai nez kas ar ikonnu
+
+
+import PySimpleGUI as sg
+# Iestatīt tēmu (vizuālo izskatu)
+sg.theme('LightGreen2') # Reddit  Dark2 Green Tan
+ 
+# Attēls pie loga nosaukuma ===*.ico
+bilde='star.ico'
+ 
+# Iekšējā funkcija, kas veic aprēķinus
+def calculate(num1, num2, operation):
+    try:
+        num1 = float(num1)
+        num2 = float(num2)
+        if operation == '+':
+            result = num1 + num2
+        elif operation == '*':
+            result = num1 * num2
+        elif operation == '-':
+            result=num1-num2
+        else:
+            result = None
+        return result
+    except ValueError:
+        return "Kļūda :("
+ 
+# Palīgmaine, lai uzglabātu ievades datus
+current_input = ''
+current_operation = ''
+ 
+# Izveidojiet GUI loga izkārtojumu
+layout = [
+    [sg.InputText(key='-DISPLAY-', size=(20, 1), justification='right', readonly=True)],
+    [sg.Button('Exit', key='-EXIT-'), sg.Button('Delete')],
+    [sg.Button('1'), sg.Button('2'), sg.Button('3')],
+    [sg.Button('4'), sg.Button('5'), sg.Button('6')],
+    [sg.Button('7'), sg.Button('8'), sg.Button('9'), sg.Button('0')],
+     [sg.Button('+'), sg.Button('-'), sg.Button('*'),sg.Button('=')],
+   
+]
+ 
+# Izveidojiet logu ar iepriekš definēto izkārtojumu
+window = sg.Window('Kalkulators', layout, resizable=True, icon=bilde)
+ 
+# Balstoties uz notikumu ciklu
+while True:
+    event, values = window.read()
+ 
+    # Ja loga aizvēršanas notikums vai Exit poga tiek nospiesta, tad iziet no cikla
+    if event == sg.WIN_CLOSED or event == '-EXIT-':
+        break
+ 
+    # Ja skaitļu poga tiek nospiesta, pievieno to skaitļu teksta ievades laukam
+    elif event in '1234567890':
+        current_input += event
+        window['-DISPLAY-'].update(current_input)
+ 
+    # Ja darbības poga tiek nospiesta, saglabā šo darbību un notīra teksta ievades lauku
+    elif event in ['+', '*', '-']:
+        current_operation = event
+        current_input = ''
+        window['-DISPLAY-'].update(current_input)
+ 
+    # Ja "C" poga tiek nospiesta, notīra teksta ievades lauku un darbību
+    elif event == 'C':
         current_input = ''
         current_operation = ''
         window['-DISPLAY-'].update(current_input)
