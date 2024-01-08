f659a974ecb59d2397c8c019dce2b2dfed83725b
Sanija Sokirka <sanijasokirka@gmail.com>
2024-01-08 09:27:10 +0200

    122334567

--- kalkulators.py
+++ kalkulators.py
@@ -2,13 +2,15 @@
 #izveidojam loga izkartojumu
 import PysimpleGUI as sg
 #paligmainigie, kuri uzglaba ievades datus
-current_imput=""
+# iekseja funkc kas veic aprekinus
+def calculate(num1,num2,opperation)
+current_input=""
 current_opperation=""
 layout=[
     [sg.InputText(key="-DISPLAY-", size=(20,1), justificator="right", readonly=True)],
     {sg.Button("1"), sg.Button("2"),sg.Button("3"),sg.Button("+")},
     {sg.Button("4"), sg.Button("5"),sg.Button("6"),sg.Button("-")},
-    {sg.Button("7"), sg.Button("8"),sg.Button("9"),sg.Button("c")},
+    {sg.Button("7"), sg.Button("8"),sg.Button("9"),sg.Button("C")},
     {sg.Button("0"), sg.Button("="),sg.Button("EXIT")}
 
 
@@ -27,7 +29,25 @@ while True:
 
     if event==sg.WIN_CLOSED or event=="EXIT":#pie pogas exit atgriezas sei tun pievieno notikumu
         break
-    elif
-    # ja ciparpoga ir nos
-    # pista tad to pievieno teksta ievades laukā
+    # ja ievada ciparu tad to piievino teksta laukam tad to pievieno teksta ievades laukā
+    elif event in "1234567890": # ja ciparpoga ir nos
+      current_input = event
+      window["-DISPLAY-"].update(current_input)
+    # ja darbibas poga ir nospiesta , saglaba darbibu un notira ievades lauku
+    elif event in "+":
+       current_opperation= event
+       current_input = event
+       window["-DISPLAY-"].update(current_input)
+    # ja nospiests "C", tad notira teksta laukā ievaditos ciparus un darbibas
+    elif event in "+":
+       current_opperation= "C"
+       current_input = event
+       window["-DISPLAY-"].update(current_input)
+
+    # ja ir nospiesta "=", javeic aprekins un jaatjauno teksta lauks aar rezultatu
+    elif event == "=":
+       rezultats= calculate(current_input,values[0], current_opperation)# kalkulate= funkcija (kurai 3 vertibas)(1. kuru ievadam, 2 masivais/ nultais elements, 3 operatorss ar ko stradasim)
+       window["-DISPLAY-"].update(current_input)
+       current_input=str(rezultats)# ko tas dos? 1.tas ko mes rekinam tas ir skaitli, japarveido uz int.
+    
     window.close()
